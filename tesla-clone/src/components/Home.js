5c216ca7f42f1f0faf2e3fcac4507b4e732647ea
Ghost620 <faghost6201@gmail.com>
2022-09-19 21:59:57 +0500

    props added

--- tesla-clone/src/components/Home.js
+++ tesla-clone/src/components/Home.js
@@ -5,7 +5,11 @@ import Section from './Section'
 function Home() {
   return (
     <Container>
-        <Section />
+        <Section title="Model-S" desc="Order Online For Touchless Delivery" img="model-s.jpg" />
+        <Section title="Model-Y" desc="Order Online For Touchless Delivery" img="model-y.jpg" />
+        <Section title="Model-X" desc="Order Online For Touchless Delivery" img="model-x.jpg" />
+        <Section title="Model-3" desc="Order Online For Touchless Delivery" img="model-3.jpg" />
+
     </Container>
   )
 }
--- tesla-clone/src/components/Section.js
+++ tesla-clone/src/components/Section.js
@@ -1,26 +1,32 @@
 import React from 'react'
 import styled from "styled-components"
 
-function Section() {
+function Section( { title, desc, img } ) {
   return (
-    <Wrap>
+    <Wrap bgImg={img}>
 
         <ItemText>
-            <h1>Model S</h1>
-            <p>Order Online for Touchless Delivery</p>
+            <h1>{title}</h1>
+            <p>{desc}</p>
         </ItemText>
 
-        <ButtonGroup>
+        <Buttons>
 
-            <LeftButton>
-                Custom Order
-            </LeftButton>
+            <ButtonGroup>
 
-            <RightButton>
-                Existing Inventory
-            </RightButton>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
 
-        </ButtonGroup>
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+
+            </ButtonGroup>
+
+            <DownArrow src="/images/down-arrow.svg" />
+
+        </Buttons>
 
     </Wrap>
   )
@@ -35,7 +41,7 @@ const Wrap = styled.div`
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
-    background-image: url('/images/model-s.jpg');
+    background-image: ${props => `url("/images/${props.bgImg}")`};
     display: flex;
     flex-direction: column;
     justify-content: space-between;  //vertical
@@ -49,6 +55,9 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
     display: flex;
     margin-bottom: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
 `
 const LeftButton = styled.div`
     background-color: rgba(23, 26,32, 0.8);
@@ -63,7 +72,18 @@ const LeftButton = styled.div`
     text-transform: uppercase;
     font-size: 12px;
     cursor: pointer;
+    margin: 8px;
 `
 const RightButton = styled(LeftButton)`
+    background: white;
+    opacity: 0.65;
+    color: black;
 
 `
+const DownArrow = styled.img`
+    height: 40px;
+    overflow-x: hidden;
+    animation: animateDown infinite 1.5s;
+`
+
+const Buttons = styled.div``
