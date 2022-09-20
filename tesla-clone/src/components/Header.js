3216b648ec56db046be5921b6234154bbdf9bc56
Ghost620 <faghost6201@gmail.com>
2022-09-20 15:28:49 +0500

    Update Header.js

--- tesla-clone/src/components/Header.js
+++ tesla-clone/src/components/Header.js
@@ -1,11 +1,64 @@
 import React from 'react'
+import styled from "styled-components"
 
 function Header() {
   return (
-    <div>
-        Header
-    </div>
+    <Container>
+        <img src='/images/logo.svg' alt='' />
+        <Menu>
+
+          <p>Model S</p>
+          <p>Model Y</p>
+          <p>Model X</p>
+          <p>Model 3</p>
+
+        </Menu>
+        <RightMenu>
+          <p>Shop</p>
+          <p>Tesla Account</p>
+        </RightMenu>
+    </Container>
   )
 }
 
 export default Header
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+`
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+
+
+  p {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+
+`
+
+const RightMenu = styled.div`
+
+  display: flex;
+  p {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px
+  }
+
+
+
+`
